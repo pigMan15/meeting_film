0d89d4be5a5325e2b40d2689c6a01ffd29da7403
pigMan15 <153306747@qq.com>
2019-11-25 22:54:53 +0800

    用户模块

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.stylefeng.guns.rest.modular.auth.controller.dto.AuthRequest;
 import com.stylefeng.guns.rest.modular.auth.controller.dto.AuthResponse;
 import com.stylefeng.guns.rest.modular.auth.util.JwtTokenUtil;
 import com.stylefeng.guns.rest.modular.auth.validator.IReqValidator;
+import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,18 +36,27 @@ public class AuthController {
 
 
     @RequestMapping(value = "${jwt.auth-path}")
-    public ResponseEntity<?> createAuthenticationToken(AuthRequest authRequest) {
+    public ResponseVO  createAuthenticationToken(AuthRequest authRequest) {
 
         userAPI.login(authRequest.getUserName(),authRequest.getPassword());
 
-        boolean validate = reqValidator.validate(authRequest);
+//        boolean validate = reqValidator.validate(authRequest);
+
+        boolean validate = true;
+        int userId = userAPI.login(authRequest.getUserName(),authRequest.getPassword());
+//        int userId = 3;
+        if(userId == 0){
+            validate = false;
+        }
 
         if (validate) {
             final String randomKey = jwtTokenUtil.getRandomKey();
-            final String token = jwtTokenUtil.generateToken(authRequest.getUserName(), randomKey);
-            return ResponseEntity.ok(new AuthResponse(token, randomKey));
+            final String token = jwtTokenUtil.generateToken(""+userId, randomKey);
+
+            return ResponseVO.success(new AuthResponse(token,randomKey));
+//            return ResponseEntity.ok(new AuthResponse(token, randomKey));
         } else {
-            throw new GunsException(BizExceptionEnum.AUTH_REQUEST_ERROR);
+            return ResponseVO.serviceFail("用户名或密码错误");
         }
     }
 }
---
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -0,0 +1,136 @@
+package com.stylefeng.guns.rest.modular.user;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocUserTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Service(interfaceClass =  UserAPI.class)
+public class UserServiceImpl implements UserAPI {
+
+    @Autowired
+    private MoocUserTMapper moocUserTMapper;
+
+
+    @Override
+    public int  login(String username, String password) {
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(username);
+        MoocUserT result = moocUserTMapper.selectOne(moocUserT);
+
+        if(result != null && result.getUuid() > 0){
+            String md5Password = MD5Util.encrypt(password);
+            if(md5Password.equals(result.getUserPwd())){
+                return result.getUuid();
+            }
+        }
+
+        return 0;
+    }
+
+    @Override
+    public boolean register(UserModel userModel) {
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(userModel.getUsername());
+        moocUserT.setEmail(userModel.getEmail());
+        moocUserT.setAddress(userModel.getAddress());
+        moocUserT.setUserPhone(userModel.getPhone());
+
+        String MD5Password = MD5Util.encrypt(userModel.getPassword());
+        moocUserT.setUserPwd(MD5Password);
+
+        Integer insert = moocUserTMapper.insert(moocUserT);
+        if(insert > 0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean checkUsername(String username) {
+        EntityWrapper<MoocUserT>  entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("user_name",username);
+        Integer count = moocUserTMapper.selectCount(entityWrapper);
+
+        System.out.println(count);
+        if(count != null && count > 0){
+
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private UserInfoModel do2UserInfo(MoocUserT moocUserT){
+        UserInfoModel userInfoModel = new UserInfoModel();
+
+        userInfoModel.setHeadAddress(moocUserT.getHeadUrl());
+        userInfoModel.setPhone(moocUserT.getUserPhone());
+        userInfoModel.setUpdateTime(moocUserT.getUpdateTime().getTime());
+        userInfoModel.setEmail(moocUserT.getEmail());
+        userInfoModel.setUsername(moocUserT.getUserName());
+        userInfoModel.setNickname(moocUserT.getNickName());
+        userInfoModel.setLifeState(""+moocUserT.getLifeState());
+        userInfoModel.setBirthday(moocUserT.getBirthday());
+        userInfoModel.setAddress(moocUserT.getAddress());
+        userInfoModel.setSex(moocUserT.getUserSex());
+        userInfoModel.setBeginTime(moocUserT.getBeginTime().getTime());
+        userInfoModel.setBiography(moocUserT.getBiography());
+
+        return userInfoModel;
+    }
+
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        MoocUserT moocUserT = moocUserTMapper.selectById(uuid);
+        UserInfoModel userInfoModel = do2UserInfo(moocUserT);
+        return userInfoModel;
+    }
+
+
+    private Date time2Date(long time){
+        return new Date(time);
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUuid(userInfoModel.getUuid());
+        moocUserT.setNickName(userInfoModel.getNickname());
+        moocUserT.setLifeState(Integer.parseInt(userInfoModel.getLifeState()));
+        moocUserT.setBirthday(userInfoModel.getBirthday());
+        moocUserT.setBiography(userInfoModel.getBiography());
+        moocUserT.setBeginTime(time2Date(userInfoModel.getBeginTime()));
+        moocUserT.setHeadUrl(userInfoModel.getHeadAddress());
+        moocUserT.setEmail(userInfoModel.getEmail());
+        moocUserT.setAddress(userInfoModel.getAddress());
+        moocUserT.setUserPhone(userInfoModel.getPhone());
+        moocUserT.setUserSex(userInfoModel.getSex());
+        moocUserT.setUpdateTime(time2Date(System.currentTimeMillis()));
+
+
+        Integer count = moocUserTMapper.updateById(moocUserT);
+        if(count > 0){
+            UserInfoModel userInfo = getUserInfo(moocUserT.getUuid());
+            return userInfo;
+        }else{
+             return userInfoModel;
+        }
+
+
+    }
+}
+
